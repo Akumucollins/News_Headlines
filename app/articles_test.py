a27cf86b5dfe223a7a849d85ac0aec549defc1e1
Akumu collins <akumucollins001@gmail.com>
2020-09-12 08:54:43 +0300

    created a class and the test behaviour file

---
+++ app/articles_test.py
@@ -0,0 +1,19 @@
+import unittest
+from app.models import Articles
+
+class ArticleTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Article class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_article = Articles('MIKE ISAAC and TAYLOR LORENZ','Sumter mirrors SC, nation in COVID-19\'s disproportionate impact on African-Americans','2020-09-12T01:20:00Z','https://edition.cnn.com/','https://www.nytimes.com/article/wildfires-photos-california-oregon-washington-state.html','News and buzz')
+
+    def test_instance(self):
+        '''
+        Test to check creation of new article instance
+        '''
+        self.assertTrue(isinstance(self.new_article,Articles))

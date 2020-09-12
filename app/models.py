0938dc4ec142c6d5e27ee559f8f124d96332dee6
Akumu collins <akumucollins001@gmail.com>
2020-09-12 08:12:28 +0300

    created a class and the test behaviour file

---
+++ app/models.py
@@ -0,0 +1,11 @@
+import app.models
+
+class Sources:
+    '''
+    Source class to define source objects
+    '''
+    def __init__(self,id,name,description,url):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
---
+++ app/source_test.py
@@ -0,0 +1,23 @@
+import unittest
+from models import Source
+from app.models import Sources
+
+class SourcesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Source class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Sources('CNN','CNN News','View the latest news and breaking news today for U.S., world, weather, entertainment, politics and health at CNN','https://www.nytimes.com/2020/09/11/us/fires-oregon-california-washington.html')
+
+    def test_instance(self):
+        '''
+        Test to check creation of new article Source instance
+        '''
+        self.assertTrue(isinstance(self.new_source,Sources))
+
+if __name__ == '__main__':
+    unittest.main()

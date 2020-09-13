c238ae2908b39bd9029732fa738e959237d16792
Akumu collins <akumucollins001@gmail.com>
2020-09-13 23:20:21 +0300

    updated the models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 class Source:
     '''
-    News class to define news objects
+    Source class to define source objects
     '''
     def __init__(self,id,name,description,url,category,language,country):
         self.id = id
@@ -10,16 +10,30 @@ class Source:
         self.category = category
         self.language = language
         self.country = country
-    
-class Headlines:
-    '''
-    News class to define News Objects
-    '''
 
-    def __init__(self, author, title, description, url, urlToImage, content):
+
+class Article:
+    '''
+    Class that instantiates objects of the news article objects of the news sources
+    '''
+    def __init__(self,id,name,author,title,description,url,urlToImage,publishedAt):
+        self.id = id
+        self.name = name
         self.author = author
         self.title = title
         self.description = description
         self.url = url
         self.urlToImage = urlToImage
-        self.content = content
+        self.publishedAt = publishedAt
+
+class Category:
+    '''
+    Class that instantiates objects of the news categories objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title

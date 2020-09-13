24d00d04b40a85d8a3bc0cc8cc6b1357f244b4be
Akumu collins <akumucollins001@gmail.com>
2020-09-13 23:30:13 +0300

    created function that gets the response to the category json, a function that  process the dictionary and output a article_list of objects

--- app/request.py
+++ app/request.py
@@ -1,33 +1,35 @@
 import urllib.request,json
-from .models import Source
+from .models import Article, Category, Source , Headlines
 
-# Getting api key
+#getting api key
 api_key = None
-# Getting source url
-source_url= None
-# Getting source url
-cat_url= None
+
+#getting news base url
+base_url = None
+category_url= None
 
 def configure_request(app):
-    global api_key, source_url
-    api_key = app.config['NEWS_API_KEY']
+    global api_key,base_url,category_url
+    api_key=app.config['NEWS_API_KEY']
+    base_url=app.config['NEWS_API_BASE_URL']
+    category_url=app.config['CATEGORY_API_URL']
 
-def get_sources():
+def get_sources(category):
     '''
-    Function that gets the json response to url request
+    Function that gets json response to our url request
     '''
-    get_sources_url= source_url.format(api_key)
+    get_sources_url = base_url.format(category,api_key)
+
     with urllib.request.urlopen(get_sources_url) as url:
         get_sources_data = url.read()
         get_sources_response = json.loads(get_sources_data)
 
-        source_results = None
+        sources_results = None
 
         if get_sources_response['sources']:
             sources_results_list = get_sources_response['sources']
             sources_results = process_sources(sources_results_list)
-
-    return source_results
+    return sources_results
 
 def process_sources(sources_list):
     '''
@@ -49,6 +51,67 @@ def process_sources(sources_list):
 
         if id:
             sources_object = Source(id,name,description,url,category,language,country)
+
             sources_results.append(sources_object)
-            
     return sources_results
+
+def article_source(id):
+    article_source_url = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'.format(id,api_key)
+    with urllib.request.urlopen(article_source_url) as url:
+        article_source_data = url.read()
+        article_source_response = json.loads(article_source_data)
+
+        article_source_results = None
+
+        if article_source_response['articles']:
+            article_source_list = article_source_response['articles']
+            article_source_results = process_articles_results(article_source_list)
+
+
+    return article_source_results
+
+def process_articles_results(articles_list):
+    '''
+    process the dictionary and output a list of objects
+    '''
+    article_source_results = []
+    source_dictionary = {}
+    for result in articles_list:
+        source_id = result ['source']
+        source_dictionary['id'] = source_id['id']
+        source_dictionary['name'] = source_id['name']
+        id = source_dictionary['id']
+        name = source_dictionary['name']
+
+        author = result.get('author')
+        title = result.get('title')
+        description = result.get('description')
+        url = result.get('url')
+        urlToImage = result.get('urlToImage')
+        publishedAt = result.get('publishedAt')
+
+        if urlToImage:
+            print (id)
+            article_object = Article(id,name,author,title,description,url,urlToImage,publishedAt)
+
+            article_source_results.append(article_object)
+
+    return article_source_results
+
+def get_category(category_name):
+    '''
+    function that gets the response to the category json
+    '''
+    get_category_url = category_url.format(category_name,api_key)
+    print(get_category_url)
+    with urllib.request.urlopen(get_category_url) as url:
+        get_category_data = url.read()
+        get_cartegory_response = json.loads(get_category_data)
+
+        get_cartegory_results = None
+
+        if get_cartegory_response['articles']:
+            get_cartegory_list = get_cartegory_response['articles']
+            get_cartegory_results = process_articles_results(get_cartegory_list)
+
+    return get_cartegory_results

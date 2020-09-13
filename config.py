fc4972b1a4a2d281c2d13a77f17cd12f8d528d87
Akumu collins <akumucollins001@gmail.com>
2020-09-13 23:33:06 +0300

    created a category url api

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_SOURCE_URL='https://newsapi.org/v2/sources?apiKey={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
     NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
+    CATEGORY_API_URL='https://newsapi.org/v2/top-headlines?country=us&category={}&apiKey={}'
 
 
 
@@ -13,6 +14,7 @@ class Config:
 class ProdConfig(Config):
     '''
     Production  configuration child class
+
     Args:
         Config: The parent configuration class with General configuration settings
     '''
@@ -22,6 +24,7 @@ class ProdConfig(Config):
 class DevConfig(Config):
     '''
     Development  configuration child class
+
     Args:
         Config: The parent configuration class with General configuration settings
     '''
